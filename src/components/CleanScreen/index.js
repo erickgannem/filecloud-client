2cf9e1261971b36e3b4ee9e5340d75648e0fba5d
Erick Gannem <erickgannem@gmail.com>
2019-05-19 20:22:14 -0400

    re-styling CleanScreen, FileItemList, FolderContent

--- src/components/CleanScreen/index.js
+++ src/components/CleanScreen/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-import CleanIcon from "../../assets/clean.svg";
 import "./style.css";
 
 export default function CleanScreen() {
   return (
     <div className="clean-screen-container">
-      <img src={CleanIcon} alt="it's clean here" className="clean-icon" />
       <h3 className="clean-text">It's empty here...</h3>
     </div>
   );
